562f3cd3153e5e43f4589441a4558ae3c4f06eb1
BorrasMiguel <miguelborras94@gmail.com>
2023-01-18 12:54:37 +0100

    React

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+    return (
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img
+                     
+                    src="https://unavatar.io/midudev" alt="El avatar de midudev" />
+                <div>
+                    <strong>Miguel Ángel Durán</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
